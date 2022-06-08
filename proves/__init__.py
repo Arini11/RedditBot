16ff6b0b7cc520cdf6e54fcc9fe11f85681def1b
Arnau Mas <57038668+Arini11@users.noreply.github.com>
2022-06-08 18:24:29 +0200

    Delete proves directory

--- proves/__init__.py
+++
@@ -1,8 +0,0 @@
-from gtts import gTTS
-frase = """Tú iluminaste mi vida,
-por eso, mujer querida
-te canto esta noche azul.
-Por eso vengo a robarte
-un rayito de tu luz."""
-tts = gTTS(frase, lang='es', tld='com.mx')
-tts.save('hola.mp3')
