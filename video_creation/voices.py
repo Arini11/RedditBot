ec1f84976df77f345625b3df40d4e2ce10f92c48
Jordi Ribellas <ribellas2002@gmail.com>
2022-06-09 20:19:25 +0200

    Veu en castellà

--- video_creation/voices.py
+++ video_creation/voices.py
@@ -17,7 +17,7 @@ def save_text_to_mp3(reddit_obj):
     # Create a folder for the mp3 files.
     Path("assets/mp3").mkdir(parents=True, exist_ok=True)
 
-    tts = gTTS(text=reddit_obj["thread_title"], lang="en", slow=False)
+    tts = gTTS(text=reddit_obj["thread_title"], lang="es", tld='com', slow=False)
     tts.save(f"assets/mp3/title.mp3")
     length += MP3(f"assets/mp3/title.mp3").info.length
 
@@ -25,7 +25,7 @@ def save_text_to_mp3(reddit_obj):
         # ! Stop creating mp3 files if the length is greater than 50 seconds. This can be longer, but this is just a good starting point
         if length > 50:
             break
-        tts = gTTS(text=comment["comment_body"], lang="en", slow=False)
+        tts = gTTS(text=comment["comment_body"], lang="es", tld='com', slow=False)
         tts.save(f"assets/mp3/{idx}.mp3")
         length += MP3(f"assets/mp3/{idx}.mp3").info.length
 
