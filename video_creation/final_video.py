40919c6ddfa5b7856ca9bf2381f965458ebf9a41
Arini11 <elarnaumc@gmail.com>
2022-06-20 17:15:47 +0200

    musica fons funciona

--- video_creation/final_video.py
+++ video_creation/final_video.py
@@ -7,6 +7,7 @@ from moviepy.editor import (
     CompositeAudioClip,
     CompositeVideoClip,
 )
+import reddit.subreddit
 import re
 from utils.console import print_step
 from dotenv import load_dotenv
@@ -25,11 +26,12 @@ def make_final_video(number_of_clips):
     VideoFileClip.reW = lambda clip: clip.resize(width=W)
     VideoFileClip.reH = lambda clip: clip.resize(width=H)
 
+    # Vídeo fons minecraft, sense àudio
     background_clip = (
         VideoFileClip("assets/mp4/clip.mp4")
-        .without_audio()
-        .resize(height=H)
-        .crop(x1=1166.6, y1=0, x2=2246.6, y2=1920),
+            .without_audio()
+            .resize(height=H)
+            .crop(x1=1166.6, y1=0, x2=2246.6, y2=1920)
     )
 
     # Gather all audio clips
@@ -40,6 +42,7 @@ def make_final_video(number_of_clips):
     try:
         audio_clips.insert(1, AudioFileClip(f"assets/mp3/posttext.mp3"))
     except:
+        print("PETA")
         OSError()
     audio_concat = concatenate_audioclips(audio_clips)
     audio_composite = CompositeAudioClip([audio_concat])
@@ -49,35 +52,43 @@ def make_final_video(number_of_clips):
     for i in range(0, number_of_clips):
         image_clips.append(
             ImageClip(f"assets/png/comment_{i}.png")
-            .set_duration(audio_clips[i + 1].duration)
-            .set_position("center")
-            .resize(width=W - 100)
-            .set_opacity(float(opacity)),
+                .set_duration(audio_clips[i + 1].duration)
+                .set_position("center")
+                .resize(width=W - 100)
+                .set_opacity(float(opacity)),
         )
     if os.path.exists(f"assets/mp3/posttext.mp3"):
         image_clips.insert(
             0,
             ImageClip(f"assets/png/title.png")
-            .set_duration(audio_clips[0].duration + audio_clips[1].duration)
-            .set_position("center")
-            .resize(width=W - 100)
-            .set_opacity(float(opacity)),
+                .set_duration(audio_clips[0].duration + audio_clips[1].duration)
+                .set_position("center")
+                .resize(width=W - 100)
+                .set_opacity(float(opacity)),
         )
     else:
         image_clips.insert(
             0,
             ImageClip(f"assets/png/title.png")
-            .set_duration(audio_clips[0].duration)
-            .set_position("center")
-            .resize(width=W - 100)
-            .set_opacity(float(opacity)),
+                .set_duration(audio_clips[0].duration)
+                .set_position("center")
+                .resize(width=W - 100)
+                .set_opacity(float(opacity)),
         )
+    #VideoClip
     image_concat = concatenate_videoclips(image_clips).set_position(
         ("center", "center")
     )
+    print("TIPUS OBJECTE IMAGE_CONCAT:" + str(type(image_concat)))
     image_concat.audio = audio_composite
     final = CompositeVideoClip([background_clip, image_concat])
     filename = (re.sub('[?\"%*:|<>]', '', ("assets/VideoFinal.mp4")))
     final.write_videofile(filename, fps=30, audio_codec="aac", audio_bitrate="192k")
-    for i in range(0, number_of_clips):
-        pass
+
+    # Sobre el vídeo final, afegir un altre audio
+    videoclip = VideoFileClip("assets/VideoFinal.mp4")
+    audioclip = AudioFileClip("assets/musica/background_music_mp3_clip.mp3")
+
+    new_audioclip = CompositeAudioClip([videoclip.audio, audioclip])
+    videoclip.audio = new_audioclip
+    videoclip.write_videofile("assets/VideoFinal2.mp4")
